4f6421515176340a3c8d0d45fe3f492563850eca
emicif <emanuela.cifolelli@gmail.com>
2022-04-04 19:19:16 +0200

    lista

--- script.js
+++ script.js
@@ -3,18 +3,29 @@ console.log('JS OK');
 const app = new Vue({
     el:  "#root",
     data: {
-        emailRandom: '',
+        emailRandom: [],
     },
     mounted(){
         console.log('server pronto', this.emailRandom);
 
        
         // axios libreria
+        for(let i = 0; i < 10; i++ ){
         axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then( (response) => {
             const result = response.data;
-            console.log('fine funzione', this.emailRandom, result);
-        });
+            //console.log('fine funzione', this.emailRandom, result);
+            console.log('Email', result.response);
+            //document.write(result.response);
+            this.emailRandom.push(result.response);
+            return this.emailRandom;
+            
+            })
+            console.log(this.emailRandom)
+        };    
+        
     }
-})
+    
+});
+
 
 
